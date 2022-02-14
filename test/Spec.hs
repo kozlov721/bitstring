4bcf6e89bd345b07e8dbc3b4d0debd10dcdcc61c
Martin Kozlovsky <kozlovsky.m7@gmail.com>
2022-02-14 03:19:56 +0100

    monoid and semigroup tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import Data.Bits
 import Data.Maybe     (fromJust)
 import Data.Word      (Word32, Word8)
 import Test.HUnit
+import Test.HUnit.Base (listAssert)
 
 import qualified Data.BitString       as BS
 import qualified Data.ByteString.Lazy as BL
@@ -34,20 +35,32 @@ simpleTests =
     ] ++
     [ "append" ~: show (x ++ y)
         ~: x ++ y ~=? BS.unpack (BS.append (BS.pack x) (BS.pack y))
-    | x <- [toBinary n | n <- [0..50]]
-    , y <- [toBinary n | n <- [0..50]]
+    | x <- [toBinary n | n <- [0..40]]
+    , y <- [toBinary n | n <- [0..40]]
     ] ++
     [ "read-show" ~: show bits
         ~: let bs = BS.pack bits in bs ~=? (read . show) bs
     | bits <- [toBinary n | n <- [0..100]]
+    ] ++
+    [ "semigroup" ~: show a ++ " <> " ++ show b ++ " <> " ++ show c
+        ~: (a <> b) <> c ~=? a <> (b <> c)
+    | a <- [BS.fromNumber n | n <- [0..10]]
+    , b <- [BS.fromNumber n | n <- [0..10]]
+    , c <- [BS.fromNumber n | n <- [0..10]]
+    ] ++
+    [ "monoid-right" ~: show bs ~: bs <> mempty ~=? bs
+    | bs <- [BS.fromNumber n | n <- [0..10]]
+    ] ++
+    [ "monoid-left" ~: show bs ~: mempty <> bs ~=? bs
+    | bs <- [BS.fromNumber n | n <- [0..10]]
     ]
 
 bitsTests = concat
     [
         [str ~: (show x ++ str ++ show y)
             ~: zipWith fint x y ~=? BS.unpack (BS.pack x `fbits` BS.pack y)
-        | x <- [toBinaryPad n | n <- [50..100]]
-        , y <- [toBinaryPad n | n <- [50..100]]
+        | x <- [toBinaryPad n | n <- [70..100]]
+        , y <- [toBinaryPad n | n <- [70..100]]
         ]
     | (fint, fbits, str) <- [ ((.&.), (.&.), " .&. ")
                             , ((.|.), (.|.), " .|. ")
@@ -60,13 +73,13 @@ bitsTests = concat
     [ "shift" ~: show b ++ " `shift` " ++ show n
         ~:  toBinaryPad (b `shift` n)
         ~=? BS.unpack (BS.pack (toBinaryPad b) `shift` n)
-    | b <- [0..100] :: [Word32]
+    | b <- [0..50] :: [Word32]
     , n <- [-10..10] :: [Int]
     ] ++
     [ "rotate" ~: show b ++ " `rotate` " ++ show n
         ~:  toBinaryPad (b `rotate` n)
         ~=? BS.unpack (BS.pack (toBinaryPad b) `rotate` n)
-    | b <- [0..100] :: [Word32]
+    | b <- [0..50] :: [Word32]
     , n <- [-10..10] :: [Int]
     ]
 
